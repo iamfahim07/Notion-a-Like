42c6b31c766cbec18a82c2b7b8e66885763d9ddc
Fahim <mfrfahim07@gmail.com>
2024-10-17 23:55:05 +0600

    Added Notion like callout functionality

--- components/editor.jsx
+++ components/editor.jsx
@@ -2,12 +2,58 @@
 
 import { useMutation } from "convex/react";
 import { useTheme } from "next-themes";
+import { RiChat1Line } from "react-icons/ri";
 
+import { Callout } from "@/blocknote-extended/callout";
 import { api } from "@/convex/_generated/api";
+import {
+  BlockNoteSchema,
+  defaultBlockSpecs,
+  filterSuggestionItems,
+  insertOrUpdateBlock,
+} from "@blocknote/core";
 import "@blocknote/core/fonts/inter.css";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
-import { useCreateBlockNote } from "@blocknote/react";
+import {
+  FormattingToolbar,
+  FormattingToolbarController,
+  SuggestionMenuController,
+  blockTypeSelectItems,
+  getDefaultReactSlashMenuItems,
+  useCreateBlockNote,
+} from "@blocknote/react";
+
+/* Our schema with block specs, which contain the configs and implementations for blocks that we want our editor to use. */
+const schema = BlockNoteSchema.create({
+  blockSpecs: {
+    // Adds all default blocks.
+    ...defaultBlockSpecs,
+    // Adds the Callout block.
+    callout: Callout,
+  },
+});
+
+// Slash menu item to insert an Alert block
+const insertAlert = (editor) => ({
+  title: "Callout",
+  onItemClick: () => {
+    insertOrUpdateBlock(editor, {
+      type: "callout",
+    });
+  },
+  aliases: [
+    "alert",
+    "notification",
+    "emphasize",
+    "warning",
+    "error",
+    "info",
+    "success",
+  ],
+  group: "Other",
+  icon: <RiChat1Line />,
+});
 
 export default function Editor({ editable, onChange, initialContent }) {
   const { resolvedTheme } = useTheme();
@@ -33,6 +79,7 @@ export default function Editor({ editable, onChange, initialContent }) {
 
   // Creates a new editor instance.
   const editor = useCreateBlockNote({
+    schema,
     initialContent: initialContent ? JSON.parse(initialContent) : undefined,
     uploadFile: handleUpload,
   });
@@ -43,7 +90,37 @@ export default function Editor({ editable, onChange, initialContent }) {
       editor={editor}
       editable={editable}
       theme={resolvedTheme === "dark" ? "dark" : "light"}
+      formattingToolbar={false}
+      slashMenu={false}
       onChange={() => onChange?.(JSON.stringify(editor.document))}
-    />
+    >
+      <FormattingToolbarController
+        formattingToolbar={() => (
+          <FormattingToolbar
+            blockTypeSelectItems={[
+              ...blockTypeSelectItems(editor.dictionary),
+              {
+                name: "Callout",
+                type: "callout",
+                icon: RiChat1Line,
+                isSelected: (block) => block.type === "callout",
+              },
+            ]}
+          />
+        )}
+      />
+
+      {/* Replaces the default Slash Menu. */}
+      <SuggestionMenuController
+        triggerCharacter={"/"}
+        getItems={async (query) =>
+          // Gets all default slash menu items and `insertAlert` item.
+          filterSuggestionItems(
+            [...getDefaultReactSlashMenuItems(editor), insertAlert(editor)],
+            query
+          )
+        }
+      />
+    </BlockNoteView>
   );
 }
